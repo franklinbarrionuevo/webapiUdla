0ba72c2a2225722848c3ec7dc779586b65f7271a
Richard <Richard@RICHARDPC>
2021-10-01 06:25:20 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/DatosUdlaController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapiUdla.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DatosUdlaController : ControllerBase
+    {
+        private static readonly string[] Ciudades = new[]
+        {
+            "Ambato", "Quito", "Riobamba", "Ibarra", "Salcedo", "Otavalo", "Latacunga", "Guaranda", "Atuntaqui", "Baños"
+        };
+
+        private readonly ILogger<DatosUdlaController> _logger;
+
+        public DatosUdlaController(ILogger<DatosUdlaController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpGet]
+        public DatosUdla GetDatosUdla()
+        {
+            var rng = new Random();
+            return new DatosUdla
+            {
+                Fecha = DateTime.Now,
+                Mensaje = "Hola Mundo",
+                Nombres = "Ricardo Barrionuevo",
+                Ciudad = Ciudades[rng.Next(Ciudades.Length)]
+            };
+        }
+    }
+}
---
+++ DatosUdla.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace webapiUdla
+{
+    //public class WeatherForecast
+    //{
+    //    public DateTime Date { get; set; }
+
+    //    public int TemperatureC { get; set; }
+
+    //    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    //    public string Summary { get; set; }
+    //}
+
+    public class DatosUdla
+    {
+        public DateTime Fecha { get; set; }
+
+        public string Mensaje { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Ciudad { get; set; }
+    }
+}
